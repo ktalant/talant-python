224e2fb3c9b203d42f8479bb28b2d997de064d27
TT KKk <uchkunfan@gmail.com>
2021-11-06 21:16:00 -0500

    Lesson2: Loops

---
+++ loop/triangle.py
@@ -0,0 +1,10 @@
+y=1
+while y <= 5:
+    x = y
+    while x <= 5:
+        print('*', end='')
+        x+=1
+    print()    
+    y+=1
+print()
+
---
+++ loop/triangle1.py
@@ -0,0 +1,10 @@
+y = 1
+
+while y != 10:
+    x = 1
+    while x <= y:
+        print('*', end='')
+        x+=1
+    print()
+    y+=1
+
